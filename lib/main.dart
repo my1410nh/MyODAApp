336b0da7672cdf02ae3328b28fc60e85639c4f79
unknown <nguyenhamy14102004@gmail.com>
2025-04-16 16:11:13 +0700

    Fix onboard

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'splash_screen.dart';
 
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Splash Screen Example',
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//       ),
-//       home: SplashScreenWithTimer(),
-//       debugShowCheckedModeBanner: false,
-//     );
-//   }
-// }
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
+  final prefs = await SharedPreferences.getInstance();
+  await prefs.remove('onboarding_complete');
 }
 
 class MyApp extends StatelessWidget {
@@ -31,9 +14,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
+    return MaterialApp(
+      title: 'Splash Screen Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       home: SplashScreenWithTimer(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
